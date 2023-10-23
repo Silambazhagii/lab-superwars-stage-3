bef2371817031bd33599db1fd46758d24a3d2c65
Silambazhagii <silambazhagii@gmail.com>
2023-10-23 11:49:59 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -23,13 +23,21 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
+    let detailedPlayers = [];
+    // Create players using for loop
+    // Type your code here
+    players.forEach((item, index) => {
+        detailedPlayers.push({
+            name: item,
+            strength: getRandomStrength(),
+            image: 'images/super-' + (index + 1) + '.png',
+            type: index % 2 == 0 ? 'hero' : 'villain',
+            id: index + 1,
+        });
+    });
     return detailedPlayers;
-}
+    // console.log(initPlayers);
+};
 
 // getting random strength
 const getRandomStrength = () => {
@@ -37,15 +45,35 @@ const getRandomStrength = () => {
 }
 
 // Build player template
-const buildPlayers = (players, type) => {
-    let fragment = '';
+const see = (play) => {
+    let player = document.createElement("div");
+    player.classList.add("player");
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
+    let div1 = document.createElement('div');
+    div1.className = 'name';
+    div1.textContent = play.name;
 
-    return fragment;
+    let div2 = document.createElement('div');
+    div2.textContent = play.strength;
+    div2.className = 'strength';
+
+    let image = document.createElement("img");
+    image.setAttribute("src", play.image);
+    image.setAttribute('alt', '');
+
+    player.append(image, div1, div2);
+    return player;
 }
+
+
+
+const buildPlayers = (players, type) => {
+    let fragment = document.createElement('div');
+    players
+        .filter((player) => player.type == type)
+        .forEach((player) => fragment.append(see(player)));
+    return fragment.innerHTML;
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
@@ -56,4 +84,62 @@ const viewPlayers = (players) => {
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
+}
+
+
+// var score = document.querySelector("#score")
+
+let array1 = [];
+let array2 = [];
+let sum1 = 0;
+let sum2 = 0;
+//   let heroscore=0;
+//   let villainscore ;
+var hs = localStorage.getItem('scoreh',hs) || 0;
+var vs = localStorage.getItem('scorev',vs) || 0;
+
+const viewPlayerss = (players) => {
+    const heroesContainer = document.getElementById("heroes");
+    const villainsContainer = document.getElementById("villains");
+
+    heroesContainer.innerHTML = buildPlayers(players, "hero");
+    villainsContainer.innerHTML = buildPlayers(players, "villain");
+    array1 = players.filter(player => player.type === "hero").map(hero => hero.strength);
+    array2 = players.filter(player => player.type === "villain").map(villain => villain.strength)
+};
+
+window.onload = () => {
+    viewPlayerss(initPlayers(PLAYERS));
+    for (let i = 0; i < array1.length; i++) {
+        sum1 += array1[i];
+    }
+
+    for (let i = 0; i < array2.length; i++) {
+        sum2 += array2[i];
+    }
+
+    if (sum1 > sum2) {
+        hs = parseInt(hs)+1;
+        updatescore();
+    }
+    else{
+        vs = parseInt(vs)+1;
+        updatescore();
+    }
+
+};
+
+var heroScore = document.querySelector("#hero-score");
+var vilainScore = document.querySelector("#villain-score");
+
+
+function updatescore() {
+
+    heroScore.innerText = hs;
+    vilainScore.innerText = vs;
+    localStorage.setItem('scoreh',hs)
+    localStorage.setItem('scorev',vs)
+
+    // console.log(temp)
+
 }
